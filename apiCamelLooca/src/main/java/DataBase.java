d3737da8efe15400d468d5afa68fba32f1535f73
JohnVSCordeiro <joao.cordeiro@sptech.school>
2023-10-22 17:18:30 -0300

    @JohnVSCordeiro, subindo att da api CamelLooca

--- apiCamelLooca/src/main/java/DataBase.java
+++ apiCamelLooca/src/main/java/DataBase.java
@@ -10,16 +10,17 @@ public class DataBase {
         String url;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-          url = "jdbc:mysql://localhost/Cameltech?user=aidmin&password=aidmin";
+            url = "jdbc:mysql://localhost/CamelTech?user=aidmin&password=aidmin";
             conexao = DriverManager.getConnection(url);
 
         } catch (SQLException error) {
-            System.out.println("Erro ao conectar ao banco de dados: " + error.getMessage());
+            System.out.println("Erro ao conectar ao banco de dados local: " + error.getMessage());
         } catch (ClassNotFoundException e) {
             System.out.println("Erro: Driver JDBC do MySQL não encontrado.");
         }
         return conexao;
     }
+
     Connection conectarNuvem() {
         Connection conexao = null;
         Boolean bdNuvem = true;
@@ -27,8 +28,8 @@ public class DataBase {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-                url="jdbc:mysql://camel-looca.mysql.database.azure.com:3306/Camel?useSSL=true";
-                conexao =DriverManager.getConnection(url, "aidminCamel", "CamelLooca1");
+            url = "jdbc:mysql://containers-us-west-156.railway.app:6470/railway";
+            conexao = DriverManager.getConnection(url, "root", "Utjrg0FbyRsc68BFOQC3");
 
         } catch (SQLException error) {
             System.out.println("Erro ao conectar ao banco de dados: " + error.getMessage());
@@ -37,52 +38,29 @@ public class DataBase {
         }
         return conexao;
     }
-    void inserirRam(Connection conexao,Double memoriaTotal, Double memoriaEmUso){
+
+    void inserir(Connection conexao, String nomeProcessador, Double usoDisco, Double tamanhoDisco, String nomeDisco, Double qtdEmUso, Long frequencia, Double totalRam, Double emUsoRam, String hostName, String numIpv4, Long bytesRecebidos, Long bytesEnviados) {
         try {
-            String insercao = "INSERT INTO dadosHardware (totalRam, emUsoRam) VALUES (?, ?)";
+            String insercao = "INSERT INTO dadosHardware (nomeProcessador,usoDisco,tamanhoDisco,nomeDisco,qtdEmUso,frequencia,totalRam,emUsoRam,hostName,numIpv4,bytesRecebidos,bytesEnviados) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement ps = conexao.prepareStatement(insercao);
-            ps.setDouble(1, memoriaTotal);
-            ps.setDouble(2, memoriaEmUso);
+
+            ps.setString(1, nomeProcessador);
+            ps.setDouble(2, usoDisco);
+            ps.setDouble(3, tamanhoDisco);
+            ps.setString(4, nomeDisco);
+            ps.setDouble(5, qtdEmUso);
+            ps.setLong(6, frequencia);
+            ps.setDouble(7, totalRam);
+            ps.setDouble(8, emUsoRam);
+            ps.setString(9, hostName);
+            ps.setString(10, numIpv4);
+            ps.setLong(11, bytesRecebidos);
+            ps.setLong(12, bytesEnviados);
+
             ps.executeUpdate();
         } catch (SQLException error) {
-            System.out.println("Erro ao inserir dados no banco de dados:" + error);
-        }
-    }
-    void inserirDisco(Connection conexao,String nomeDisco, Double tamanhoDisco, Double usoDisco){
-        try {
-            String insercao =  "INSERT INTO dadosHardware (nomeDisco,tamanhoDisco,usoDisco) VALUES (?, ?, ?)";
-            PreparedStatement ps = conexao.prepareStatement(insercao);
-            ps.setString(1,nomeDisco);
-            ps.setDouble(2,tamanhoDisco);
-            ps.setDouble(3,usoDisco);
-            ps.executeUpdate();
-        } catch (SQLException error) {
-            System.out.println("Erro ao inserir dados no banco de dados:" + error);
-        }
-    }
-    void inserirRede(Connection conexao, String hostName, String numIpv4, Long bytesRecebidos, Long bytesEnviados){
-        try {
-            String insercao =  "INSERT INTO dadosHardware (hostName,numIpv4,bytesRecebidos,bytesEnviados) VALUES (?, ?, ?,?)";
-            PreparedStatement ps = conexao.prepareStatement(insercao);
-            ps.setString(1,hostName);
-            ps.setString(2,numIpv4);
-            ps.setLong(3,bytesRecebidos);
-            ps.setLong(4,bytesEnviados);
-            ps.executeUpdate();
-        } catch (SQLException error) {
-            System.out.println("Erro ao inserir dados no banco de dados:" + error);
-        }
-    }
-    void inserirProcessador(Connection conexao, String nomeProcessador,Double qtdEmUSo, Long frequancia ){
-        try {
-            String insercao =  "INSERT INTO dadosHardware (nomeProcessador,qtdEmUso,frequencia) VALUES (?,?,?)";
-            PreparedStatement ps = conexao.prepareStatement(insercao);
-            ps.setString(1,nomeProcessador);
-            ps.setDouble(2,qtdEmUSo);
-            ps.setLong(3,frequancia);
-            ps.executeUpdate();
-        } catch (SQLException error) {
-            System.out.println("Erro ao inserir dados no banco de dados:" + error);
+            System.out.println("Erro ao inserir dados no banco de dados: " + error);
         }
     }
 }
+
--- apiCamelLooca/src/main/java/Main.java
+++ apiCamelLooca/src/main/java/Main.java
@@ -8,6 +8,9 @@ import java.util.TimerTask;
 public class Main {
     public static void main(String[] args) {
 
+
+
+
         MemoriaDeDisco disco = new MemoriaDeDisco();
         MemoriaRam ram = new MemoriaRam();
         PlacaDeRede rede = new PlacaDeRede();
@@ -15,7 +18,7 @@ public class Main {
         DataBase db = new DataBase();
         Timer timer = new Timer();
         Connection conexao = db.conectar();
-//        Connection conexaoNuvem = db.conectarNuvem();
+        Connection conexaoNuvem = db.conectarNuvem();
         System.out.println("Inicio do processo de captura");
         System.out.println("Tempo total de captura: 50m");
         for (int i = 0; i < 100; i++) {
@@ -28,15 +31,14 @@ public class Main {
                     ram.captarMemoria();
                     rede.captarPlacaDeRede();
                     processador.captarProcessador();
-                    db.inserirRam(conexao, ram.getMemoriaTotal(), ram.getMemoriaEmUso());
-                    db.inserirDisco(conexao, disco.getNome(), disco.getTamanho(), disco.getUsoDisco());
-                   db.inserirRede(conexao, rede.getHostName(), rede.getNumIpv4(), rede.getBytesRecebidos(), rede.getBytesEnviados());
-                    db.inserirProcessador(conexao,processador.getNome(),processador.getQtdemUso(), processador.getFrequencia());
 
-//                    db.inserirRam(conexaoNuvem, ram.getMemoriaTotal(), ram.getMemoriaEmUso(), ram.getMemoriaDisponivel());
-//                    db.inserirDisco(conexaoNuvem, disco.getNome(), disco.getModelo(), disco.getTamanho(), disco.getVelocidadeLeitura(), disco.getLeituras(), disco.getVelocidadeEscrita(), disco.getEscritas(), disco.getFila(), disco.getTempoTransferencia());
-//                    db.inserirRede(conexaoNuvem, rede.getHostName(), rede.getNomeDeDominio(), Arrays.toString(rede.getServidoresDNS()));
-//                    db.inserirProcessador(conexaoNuvem, processador.getFabricante(), processador.getNome(), processador.getNumCpuFisica(), processador.getNumCpuLogica(), processador.getQtdemUso(), processador.getFrequencia());
+                    db.inserir(conexao, processador.getNome(), disco.getUsoDisco(), disco.getTamanho(), disco.getNome(), disco.getUsoDisco(), processador.getFrequencia(), ram.getMemoriaTotal(), ram.getMemoriaEmUso(), rede.getHostName(), rede.getNumIpv4(), rede.getBytesRecebidos(), rede.getBytesEnviados());
+
+                    disco.captarInformacoesDoDisco();
+                    ram.captarMemoria();
+                    rede.captarPlacaDeRede();
+                    processador.captarProcessador();
+                   db.inserir(conexaoNuvem, processador.getNome(), disco.getUsoDisco(), disco.getTamanho(), disco.getNome(), disco.getUsoDisco(), processador.getFrequencia(), ram.getMemoriaTotal(), ram.getMemoriaEmUso(), rede.getHostName(), rede.getNumIpv4(), rede.getBytesRecebidos(), rede.getBytesEnviados());
                 }
             }, i * 30000);
         }
